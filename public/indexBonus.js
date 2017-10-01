2a504bd6c23b3ff1961dc5e5960bb5432ed4b3bb
Combii <davidstovle@gmail.com>
2017-10-01 22:43:35 +0200

    Working hide background color button

--- public/indexBonus.js
+++ public/indexBonus.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     $("#changeColorBtn").click(function(){
-        $('*').css('background', 'transparent');
+        $("*").css('background', 'transparent');
+        $("#changeColorBtn").hide();
     });
 });
